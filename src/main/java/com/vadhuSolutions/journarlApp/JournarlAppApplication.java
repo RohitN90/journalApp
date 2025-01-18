b082980d953573afed74637e4bfa29f2d4565f03
rohitvardhan <rohitvardhan88@gmail.com>
2025-01-18 16:36:17 +0530

    Added spring boot Journal App (Backend API'S) files

---
+++ src/main/java/com/vadhuSolutions/journarlApp/JournarlAppApplication.java
@@ -0,0 +1,13 @@
+package com.vadhuSolutions.journarlApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournarlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournarlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadhuSolutions/journarlApp/JournarlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadhuSolutions.journarlApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournarlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
